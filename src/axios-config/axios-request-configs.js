47e521b11acfccccecf2b9a27003da40f0681671
romulofss <romulo.fernando@paytrack.com.br>
2021-03-08 10:30:52 -0300

    Consumir apis do heroku

--- src/axios-config/axios-request-configs.js
+++ src/axios-config/axios-request-configs.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import auth from "../authentication/auth";
 
-const baseURL = "http://localhost:3333/";
+// const baseURL = "http://localhost:3333/";
+const baseURL = "https://crud-romulo-backend.herokuapp.com/";
 const objAxios = axios.create({ baseURL });
 
 auth.setAxiosInstance(objAxios);
